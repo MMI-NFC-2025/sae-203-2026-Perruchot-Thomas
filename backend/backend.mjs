a9acd5fdff44456e47bdcf31b682c9b0169732ea
Perruchot-Thomas <thomas.perruchot@edu.univ-fcomte.fr>
2026-03-22 15:49:36 +0100

    correction lien

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
-const pb = new PocketBase('https://echosonore.perruchot.optimiseus.fr');
+const pb = new PocketBase('http://127.0.0.1:8090');
 
 
 export async function getAllArtistes() {
